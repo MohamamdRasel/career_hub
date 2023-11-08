12afbabe9e02734ca606778f80acc748c9f113e7
Mohammad <mohammadrasel201@gmail.com>
2023-11-08 18:50:19 +0600

    Added hidden button and event Handler

--- src/components/FeaturedJobs/FeaturedJobs.jsx
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -5,6 +5,8 @@ import Job from "../Job/Job";
 const FeaturedJobs = () => {
 
     const [jobs, setJobs] = useState([]);
+
+    const [dataLength, setDataLength] = useState(4);
     useEffect ( () =>{
         fetch('jobs.json')
         .then(res => res.json())
@@ -21,8 +23,13 @@ const FeaturedJobs = () => {
 
             <div className="grid grid-cols-2 gap-6">
                 {
-                    jobs.map(job => <Job key={job.id} job={job}></Job>)
+                    jobs.slice(0, dataLength).map(job => <Job key={job.id} job={job}></Job>)
                 }
+            </div>
+            <div className={ dataLength === jobs.length ? 'hidden' : ''}>
+                <button
+                    onClick={() => setDataLength(jobs.length)}
+                    className="btn btn-primary">Show All Jobs</button>
             </div>
         </div>
     );
